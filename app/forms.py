b98d7a0492e4721968ed76da08c3839e5bdc6cdd
Sebastian Power <sebastianrosspower@gmail.com>
2024-12-24 12:38:41 +1100

    Login and Signup pages completed

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, EmailField, PasswordField
-from wtforms.validators import DataRequired, Length
+from wtforms import SubmitField, EmailField, PasswordField, StringField, SelectField
+from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(FlaskForm):
     email = EmailField("Email", validators=[DataRequired(message="This field is required")], render_kw={"placeholder": "Enter your email"})
     password = PasswordField("Password", validators=[DataRequired(message="This field is required")], render_kw={"placeholder": "Enter your password"})
     submit = SubmitField("Continue")
+
+class SignupForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired(message="This field is required"), Length(max=100)], render_kw={"placeholder": "Enter your name"})
+    email = EmailField("Email", validators=[DataRequired(message="This field is required"), Email(message="Please enter a valid email"), Length(max=100)], render_kw={"placeholder": "Enter your email"})
+    role = SelectField("Role", validators=[DataRequired(message="This field is required")], choices=[("Member", "Member"), ("Worker", "Worker"), ("Admin", "Admin")])
+    role_pwd = StringField("Role Password", validators=[DataRequired(message="This field is required")], render_kw={"placeholder": "Enter the role password"})
+    password = PasswordField("Password", validators=[DataRequired(message="This field is required")], render_kw={"placeholder": "Enter your password"})
+    submit = SubmitField("Create an account")
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect
-from .forms import LoginForm
+from .forms import LoginForm, SignupForm
 
 main_bp = Blueprint("main", __name__)
 
@@ -9,7 +9,7 @@ def home_page():
     """
     return redirect("/login")
 
-@main_bp.route('/login')
+@main_bp.route("/login")
 def login():
     """Render's the log in page
 
@@ -22,3 +22,16 @@ def login():
         password = form.password.data
         print(name, password)
     return render_template("login.html", form=form)
+
+@main_bp.route("/signup")
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        name = form.name
+        email = form.email
+        role = form.role
+        role_pwd = form.role_pwd
+        password = form.password
+    return render_template("signup.html", form=form)
+        
+    
